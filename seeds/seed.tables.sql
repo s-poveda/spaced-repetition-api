afdfa9979b6e2e53bd21d559ff3e16c341406cc1
sam p <spoveda545@gmail.com>
2020-12-18 16:00:29 -0500

    updated seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,4 +1,7 @@
 BEGIN;
+-- IMPORTANT: the system hosting this db MUST be using UTF8 encoding
+-- for windows users: Enable "Beta: Use UTF8 encoding for global language support" in advanced Region settings
+SET client_encoding TO 'UTF8';
 
 TRUNCATE
   "word",
@@ -17,18 +20,18 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Japanese', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, '練習する', 'to practice', 2),
+  (2, 1, 'こんにちは', 'hello', 3),
+  (3, 1, '家', 'house', 4),
+  (4, 1, '開発者', 'developer', 5),
+  (5, 1, '散歩する', 'to take a walk', 6),
+  (6, 1, '凄い', 'amazing', 7),
+  (7, 1, '犬', 'dog', 8),
+  (8, 1, '猫', 'cat', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
